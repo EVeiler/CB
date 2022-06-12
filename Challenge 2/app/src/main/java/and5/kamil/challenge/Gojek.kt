920712bf6f78d9f2eb9ab5514335e61ed892c199
EVeiler <107327280+EVeiler@users.noreply.github.com>
2022-06-12 18:19:17 +0700

    Add files via upload

---
+++ Challenge 2/app/src/main/java/and5/kamil/challenge/Gojek.kt
@@ -0,0 +1,11 @@
+package and5.kamil.challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Gojek : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gojek)
+    }
+}
